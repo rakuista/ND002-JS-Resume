98aefb95ef29b00cf96b377f4d40bc9b01532291
rakuista <rakuista@gmail.com>
2014-10-21 22:20:00 -0700

    Created data objects in JS file

---
+++ js/resumeBuilder.js
@@ -0,0 +1,88 @@
+var work = {
+    "jobs" : [
+        {
+            "employer" : "Intuit",
+            "title" : "Communications Manager",
+            "location" : "Mountain View, CA",
+            "dates" : "2013–Present",
+            "description" : "Internal product marketing for platform APIs and services."
+        },
+        {
+            "employer" : "TheIcebreak",
+            "title" : "Editorial Director, UI Designer",
+            "location" : "Mountain View, CA",
+            "dates" : "2011-2012",
+            "description" : "Multiple roles for a relationship app startup."
+        },
+        {
+            "employer" : "YouTube/Google",
+            "title" : "Lead UX Writer, Technical Writer",
+            "location" : "San Bruno, CA",
+            "dates" : "2006–2011",
+            "description" : "UX & technical writing, localization project management, UI design. Pre-acquisition hire."
+        }
+    ]
+}
+
+var projects = {
+    "projects" : [
+        {
+            "title" : "Refuge Restrooms",
+            "dates" : "2013",
+            "description" : "Hackathon project from Lesbians Who Code conference. I contributed a little Ruby code and designed PDFs of restroom signs.",
+            "images" : ["images/rr-001.png","images/rr-002.png"]
+        },
+        {
+            "title" : "imgix Documentation",
+            "dates" : "2013",
+            "description" : "Wrote documentation for signup process and v1 of API.",
+            "images" : ["images/iix-001.png","images/iix-002.png"]
+        }
+    ]
+}
+
+var bio = {
+    "name" : "Carol Gunby",
+    "role" : "Web Developer",
+    "welcomeMessage" : "Hi, nice to meet me.",
+    "contacts" : {
+        "mobile" : "408-464-7147",
+        "email" : "rakuista@gmail.com",
+        "github": "rakuista",
+        "twitter" : "rakuista",
+        "location" : "San Jose, CA"
+    },
+    "skills" : ["HTML", "CSS", "JavaScript/jQuery", "Adobe Creative Suite", "Git"]
+}
+
+var education = {
+    "schools" : [
+        {
+            "name" : "UC Berkeley Extension",
+            "location" : "Berkeley, CA",
+            "degree" : "Professional Certificate, Copyediting",
+            "majors" : ["n/a"],
+            "dates" : "2011-2012"
+        },
+        {
+            "name" : "Pacifica Graduate Institute",
+            "location" : "Carpinteria, CA",
+            "degree" : "MA magna cum laude)",
+            "majors" : ["Engaged Humanities"],
+            "dates" : "2008–2010"
+        }
+   ],
+    "onlineCourses" : [
+        {
+            "onlineCourse" : [
+                {
+                    "title" : "UX Design Immersive",
+                    "school" : "General Assembly",
+                    "dates" : "2013",
+                    "url" : "http://generalassemb.ly"
+                }
+            ]
+        }
+    ]
+}
+
